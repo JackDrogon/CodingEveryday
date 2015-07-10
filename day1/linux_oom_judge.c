b8657886458b5f13c8bdc0a1ee29ca289ac3294d
JackDrogon <jack.xsuperman@gmail.com>
2015-07-10 20:36:50 +0800

    Change linux_oom_judge.c code style of day1

--- day1/linux_oom_judge.c
+++ day1/linux_oom_judge.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <time.h>
 #include <stdarg.h>
+#include <time.h>
 
 /* Log levels */
 #define REDIS_DEBUG 0
@@ -12,51 +12,59 @@
 struct __log_setting {
 	char *logfile;
 	uint8_t verbosity;
-} log_setting = {.logfile = NULL, .verbosity = REDIS_DEBUG};
+} log_setting = { .logfile = NULL, .verbosity = REDIS_DEBUG };
 
-static void redisLog(int level, const char *fmt, ...) {
-    va_list ap;
-    FILE *fp;
+static void redisLog(int level, const char *fmt, ...)
+{
+	va_list ap;
+	FILE *fp;
 
-    fp = (log_setting.logfile == NULL) ? stdout : fopen(log_setting.logfile, "a");
-    if (!fp) return;
+	fp = (log_setting.logfile == NULL) ? stdout : fopen(log_setting.logfile,
+							    "a");
+	if (!fp)
+		return;
 
-    va_start(ap, fmt);
-    if (level >= log_setting.verbosity) {
-        char *c = ".-*";
-        char buf[64];
-        time_t now;
+	va_start(ap, fmt);
+	if (level >= log_setting.verbosity) {
+		char *c = ".-*";
+		char buf[64];
+		time_t now;
 
-        now = time(NULL);
-        strftime(buf,64,"%d %b %H:%M:%S",localtime(&now));
-        fprintf(fp,"%s %c ",buf,c[level]);
-        vfprintf(fp, fmt, ap);
-        fprintf(fp,"\n");
-        fflush(fp);
-    }
-    va_end(ap);
+		now = time(NULL);
+		strftime(buf, 64, "%d %b %H:%M:%S", localtime(&now));
+		fprintf(fp, "%s %c ", buf, c[level]);
+		vfprintf(fp, fmt, ap);
+		fprintf(fp, "\n");
+		fflush(fp);
+	}
+	va_end(ap);
 
-    if (log_setting.logfile) fclose(fp);
+	if (log_setting.logfile)
+		fclose(fp);
 }
 
-int linuxOvercommitMemoryValue(void) {
-    FILE *fp = fopen("/proc/sys/vm/overcommit_memory","r");
-    char buf[64];
+int linuxOvercommitMemoryValue(void)
+{
+	FILE *fp = fopen("/proc/sys/vm/overcommit_memory", "r");
+	char buf[64];
 
-    if (!fp) return -1;
-    if (fgets(buf,64,fp) == NULL) {
-        fclose(fp);
-        return -1;
-    }
-    fclose(fp);
+	if (!fp)
+		return -1;
+	if (fgets(buf, 64, fp) == NULL) {
+		fclose(fp);
+		return -1;
+	}
+	fclose(fp);
 
-    return atoi(buf);
+	return atoi(buf);
 }
 
-void linuxOvercommitMemoryWarning(void) {
-    if (linuxOvercommitMemoryValue() == 0) {
-        redisLog(REDIS_WARNING,"WARNING overcommit_memory is set to 0! Background save may fail under low condition memory. To fix this issue add 'vm.overcommit_memory = 1' to /etc/sysctl.conf and then reboot or run the command 'sysctl vm.overcommit_memory=1' for this to take effect.");
-    }
+void linuxOvercommitMemoryWarning(void)
+{
+	if (linuxOvercommitMemoryValue() == 0) {
+		redisLog(REDIS_WARNING,
+			 "WARNING overcommit_memory is set to 0! Background save may fail under low condition memory. To fix this issue add 'vm.overcommit_memory = 1' to /etc/sysctl.conf and then reboot or run the command 'sysctl vm.overcommit_memory=1' for this to take effect.");
+	}
 }
 
 int main()
