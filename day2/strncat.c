3d62cf359496057841b8e7cae2e3bf3195e46b19
JackDrogon <jack.xsuperman@gmail.com>
2015-07-10 20:34:03 +0800

    Add strncat in day2

--- day2/strncat.c
+++ day2/strncat.c
@@ -10,3 +10,16 @@ char *strncat(char *restrict dest, const char *restrict src, size_t n)
 
 	return dest;
 }
+
+// 下面这点写法效率会比较低
+// char* strncat(char *dest, const char *src, size_t n)
+// {
+// 	size_t dest_len = strlen(dest);
+// 	size_t i;
+// 
+// 	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+// 	dest[dest_len + i] = src[i];
+// 	dest[dest_len + i] = '\0';
+// 
+// 	return dest;
+// }
