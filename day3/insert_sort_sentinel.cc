31302b8b8532fb4dea920c6beb7f2686ab401d36
JackDrogon <jack.xsuperman@gmail.com>
2015-07-11 21:58:47 +0800

    Add insert sort by using sentinel in day3

---
+++ day3/insert_sort_sentinel.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+void print_arr(int arr[], size_t n);
+void insert_sort(int arr[], const size_t n);
+
+int main()
+{
+	int arr[15] = {1, 3, 6, 7, 8, 9, 10, 2, 5, 4, 14, 12, 13, 11};
+
+	cout << "Before Insert Sort, Array: ";
+	print_arr(arr, 15);
+
+	insert_sort(arr, 15);
+
+	cout << "After Insert Sort, Array: ";
+	print_arr(arr, 15);
+
+	return 0;
+}
+
+void print_arr(int arr[], size_t n){
+	for (size_t i = 0; i < n; ++i) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+void insert_sort(int arr[], const size_t n)
+{
+	if (n <= 1) return;
+	for (size_t i = 1; i < n; ++i) {
+		int sentinel = arr[i];
+		size_t j;
+		for (j = i; j > 0 && arr[j-1] > sentinel; --j) {
+			arr[j] = arr[j-1];
+		}
+		arr[j] = sentinel;
+	}
+}
