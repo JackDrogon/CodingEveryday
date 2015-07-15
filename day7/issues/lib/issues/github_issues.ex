51a13c8c7e98d8d33f886175b280457485a2508c
JackDrogon <jack.xsuperman@gmail.com>
2015-07-15 18:32:28 +0800

    Add issues dep httpoison in day7

---
+++ day7/issues/lib/issues/github_issues.ex
@@ -0,0 +1,18 @@
+defmodule Issues.GithubIssues do
+  
+  @user_agent [ {"User-agent", "Jack Drogon"}]
+
+  def fetch(user, project) do
+    issues_url(user, project)
+      |> HTTPoison.get(@user_agent)
+      |> handle_response
+  end
+
+  def issues_url(user, project) do
+    "https://api.github.com/repos/#{user}/#{project}/issues"
+  end
+
+  def handle_response({:ok, %{status_code: 200, body: body}}), do: {:ok, body}
+  def handle_response({:error, %HTTPoison.Response{status_code: _, body: body}}), do: {:error, body}
+
+end
