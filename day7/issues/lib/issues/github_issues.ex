24f1fa99ffbf414e4f30cc10953b1fa25264b040
JackDrogon <jack.xsuperman@gmail.com>
2015-07-15 23:20:04 +0800

    Add logger in issues in day7

--- day7/issues/lib/issues/github_issues.ex
+++ day7/issues/lib/issues/github_issues.ex
@@ -1,9 +1,12 @@
 defmodule Issues.GithubIssues do
+
+  require Logger
   
   @user_agent [ {"User-agent", "Jack Drogon"}]
   @github_url Application.get_env(:issues, :github_url)
 
   def fetch(user, project) do
+    Logger.info "Fetching user #{user}'s project #{project}"
     issues_url(user, project)
       |> HTTPoison.get(@user_agent)
       |> handle_response
@@ -13,7 +16,14 @@ defmodule Issues.GithubIssues do
     "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
-  def handle_response({:ok, %{status_code: 200, body: body}}), do: {:ok, :jsx.decode(body)}
-  def handle_response({:error, %HTTPoison.Response{status_code: _, body: body}}), do: {:error, :jsx.decode(body)}
+  def handle_response({:ok, %{status_code: 200, body: body}}) do
+    Logger.info "Successful response"
+    Logger.debug fn -> inspect(body) end
+    {:ok, :jsx.decode(body)}
+  end
+  def handle_response({:error, %HTTPoison.Response{status_code: status, body: body}}) do
+    Logger.error "Error #{status} returned"
+    {:error, :jsx.decode(body)}
+  end
 
 end
