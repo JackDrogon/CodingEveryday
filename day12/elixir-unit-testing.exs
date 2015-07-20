b960a3abc0634d7fa03c35290d3a8a4c1590ba1d
JackDrogon <jack.xsuperman@gmail.com>
2015-07-20 17:19:13 +0800

    Add elixir unit testing example in day12

---
+++ day12/elixir-unit-testing.exs
@@ -0,0 +1,38 @@
+#!/usr/bin/env elixir
+
+# http://elixir-lang.org/getting-started/mix-otp/introduction-to-mix.html
+# http://elixir-lang.org/docs/stable/ex_unit/ExUnit.html
+
+ExUnit.start                                # set up the test runner
+
+defmodule MyTest do
+
+  use ExUnit.Case                           # use requires a module and sets up macros; will explore more later
+
+  test 'simple test' do
+    assert 1 + 1 == 2
+    #assert 1 + 1 == 3                      # Elixir is smart! No need for assert_equal, assert_gte, etc.
+                                            # And we still get great failure messages, yipee!
+                                            # 1) test_assert (MyTest)
+                                            #    ** (ExUnit.ExpectationError)
+                                            #                 expected: 2
+ 
+  end
+
+  test "refute is opposite of assert" do    # test macro accepts string as test name
+    refute 1 + 1 == 3
+  end
+  
+  test :assert_raise do                     # test macro also accepts an atom
+    assert_raise ArithmeticError, fn ->
+      1 + "x"
+    end
+  end
+
+  test "assert_in_delta asserts that val1 and val2 differ by no more than delta." do
+    assert_in_delta 1, # actual
+                    5, # expected
+                    6  # delta
+  end
+ 
+end
