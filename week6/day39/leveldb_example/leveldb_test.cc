2bc9910221a7b0ed9185ed97293ca827a927aa81
JackDrogon <jack.xsuperman@gmail.com>
2015-08-16 16:00:52 +0800

    Add leveldb example in day39

---
+++ week6/day39/leveldb_example/leveldb_test.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <leveldb/db.h>
+#include <cassert>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	if (argc <= 2) {
+		cout << "Usage: " << argv[0] << " key " << "value" << endl;
+		return 0;
+	}
+
+	leveldb::DB *db;
+	leveldb::Options options;
+
+	options.create_if_missing = true;
+
+	leveldb::Status status = leveldb::DB::Open(options, "/tmp/testdb", &db);
+	assert(status.ok());
+
+	//string key = "MyKey29", value="Hello,World!", result;
+	string key = argv[1], value=argv[2], result;
+	db->Put(leveldb::WriteOptions(), key, value);
+
+	db->Get(leveldb::ReadOptions(), key, &result);
+
+	cout << "result = " << result << endl;
+
+	delete db;
+
+	return 0;
+}
+
+// vim: se ts=8 sw=8
