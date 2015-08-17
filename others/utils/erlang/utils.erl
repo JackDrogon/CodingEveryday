0af084a0738b86876e7ecdebbbe3258372eb4ca6
JackDrogon <jack.xsuperman@gmail.com>
2015-08-17 19:05:29 +0800

    Update erlang utils

--- others/utils/erlang/utils.erl
+++ others/utils/erlang/utils.erl
@@ -2,8 +2,16 @@
 -author('Drogon').
 
 -export([
-         print_line/1
+         print_line/1,
+         print_str_line/1,
+         pid_status/1
         ]).
 
 print_line(X) ->
     io:format("~p~n", [X]).
+
+print_str_line(X) ->
+    io:format("~s~n", [X]).
+
+pid_status(Pid) ->
+    sys:get_status(Pid).
