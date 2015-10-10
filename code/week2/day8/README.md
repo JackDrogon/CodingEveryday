9cc209cf9aeed8cd152f01ec5ab2787ff25c8318
JackDrogon <jack.xsuperman@gmail.com>
2015-10-10 23:20:54 +0800

    Add day8 with chrono_sleep example

---
+++ code/week2/day8/README.md
@@ -0,0 +1,5 @@
+## Day One
+### Today is 2015-10-10, Sat.
+
+#### My Code
+1. chrono_sleep.cc
---
+++ code/week2/day8/chrono_sleep.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+int main()
+{
+	using namespace std::literals;
+
+	std::cout << "Hello waiter" << std::endl;
+
+	auto start = std::chrono::high_resolution_clock::now();
+	/* std::this_thread::sleep_for(std::chrono::seconds(1)); */
+	std::this_thread::sleep_for(1.2s); // by using std::literals, 研究一下, 类似还有bind的_1
+
+	// constexpr chrono::seconds operator"" s(unsigned long long __s)
+	// {
+		// return chrono::seconds(static_cast<chrono::seconds::rep>(__s));
+	// }
+	
+	auto end = std::chrono::high_resolution_clock::now();
+
+	std::chrono::duration<double, std::milli> elapsed = end - start;
+
+	std::cout << "Waited " << elapsed.count() << " ms\n";
+
+	return 0;
+}
