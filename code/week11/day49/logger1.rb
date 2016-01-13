db18bbc116495a068d055b6c746017f7a7eb76f5
JackDrogon <jack.xsuperman@gmail.com>
2016-01-13 17:47:03 +0800

    Add ruby log examples in day49

---
+++ code/week11/day49/logger1.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+# -*- coding:utf-8 -*-
+
+require 'logger'
+
+logger = Logger.new STDOUT
+logger.level = Logger::WARN
+
+logger.debug("Created logger")
+logger.info("Program started")
+logger.warn("Nothing to do!")
+
+path = "a_non_existent_file"
+
+begin
+  File.foreach(path) do |line|
+    unless line =~ /^(\w+) = (.*)$/
+      logger.error("Line in wrong format: #{line.chomp}")
+    end
+  end
+rescue => err
+  logger.fatal("Caught exception; exiting")
+  logger.fatal(err)
+end
