2c75e4e6653494f5ed58147a882f10d034ad917c
JackDrogon <jack.xsuperman@gmail.com>
2016-01-21 19:42:51 +0800

    Add some TODOs in base64 of cpp utils

--- others/utils/cpp/base64/base64.h
+++ others/utils/cpp/base64/base64.h
@@ -3,9 +3,12 @@
 
 #include <string>
 
+// TODO: Add mime_encode && mime_decode
+// TODO: Think about decode BAD error && = in middle problem(ex: "\t       SGVsbG\t     8s================V29ybGQh\t=======")
+
 namespace base64 {
 	std::string encode(const std::string& src);
 	std::string decode(const std::string& src);
-} // base64
+} // namespace base64
 
 #endif // BASE64_H_
