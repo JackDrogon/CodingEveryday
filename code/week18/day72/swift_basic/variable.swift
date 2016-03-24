f9f0494680d457cdeca74d7c5298208c93b40c61
JackDrogon <jack.xsuperman@gmail.com>
2016-03-24 17:08:12 +0800

    Add some basic swift examples in day72

---
+++ code/week18/day72/swift_basic/variable.swift
@@ -0,0 +1,9 @@
+// Constant Variable
+let π = 3.14159
+let 你好 = "你好世界"
+let 🐶🐮 = "dogcow"
+
+// Variable
+var friendlyWelcome = "Hello!"
+friendlyWelcome = "Bonjour!"
+print("The current value of friendlyWelcome is \(friendlyWelcome)")
