efea1ba0909400a95af36e02837f169cefda5edb
JackDrogon <jack.xsuperman@gmail.com>
2016-05-30 14:03:46 +0800

    Add cpp socket lib socket implement

--- others/utils/cpp/socket/socket.cc
+++ others/utils/cpp/socket/socket.cc
@@ -1,12 +1,79 @@
 #include "socket.h"
+#include "inet_address.h"
 
+#include <cstdlib>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <netinet/tcp.h>
+#include <sys/errno.h>
+
+namespace nepenthe {
+
+
+Socket::Socket(int type, int protocol)
+	: sockfd_(::socket(AF_INET, type, protocol))
+{
+}
 
 Socket::~Socket() { ::close(sockfd_); }
+
+int Socket::Close() { return ::close(sockfd_); }
+
+int Socket::Bind(const InetAddress &address)
+{
+	return ::bind(sockfd_, reinterpret_cast<const struct sockaddr *>(address.SockAddr()), address.Size());
+}
+
+int Socket::Connect(const InetAddress &address)
+{
+	// ::connect(sockfd_, &(const_cast<InetAddress &>(address).SockAddr()), const_cast<InetAddress &>(address).Size());
+	return ::connect(sockfd_, reinterpret_cast<const struct sockaddr *>(address.SockAddr()), address.Size());
+}
+
+
+int Socket::Listen(int backlog)
+{
+	return ::listen(sockfd_, backlog);
+}
+
+std::shared_ptr<Socket> Socket::Accept(InetAddress &client_address)
+{
+	socklen_t addrlen = client_address.Size();
+	int client_fd = ::accept(sockfd_, reinterpret_cast<struct sockaddr *>(client_address.SockAddr()), &addrlen);
+
+	if (client_fd < 0) {
+		int saved_errno = errno;
+		switch (saved_errno) {
+			case EAGAIN:
+			case ECONNABORTED:
+			case EINTR:
+			case EPROTO: // ???
+			case EPERM:
+			case EMFILE: // per-process lmit of open file desctiptor ???
+				// expected errors
+				errno = saved_errno;
+				break;
+			case EBADF:
+			case EFAULT:
+			case EINVAL:
+			case ENFILE:
+			case ENOBUFS:
+			case ENOMEM:
+			case ENOTSOCK:
+			case EOPNOTSUPP:
+				// unexpected errors
+				break;
+			default:
+				break;
+		}
+
+		return std::shared_ptr<Socket>();
+	}
+
+	return std::make_shared<Socket>(client_fd);
+}
 
 void Socket::SetTcpNoDelay(bool on)
 {
@@ -31,11 +98,13 @@ void Socket::SetReusePort(bool on)
 	int ret = ::setsockopt(sockfd_, SOL_SOCKET, SO_REUSEPORT, &optval,
 			       static_cast<socklen_t>(sizeof optval));
 	if (ret < 0 && on) {
-		LOG_SYSERR << "SO_REUSEPORT failed.";
+		// LOG_SYSERR << "SO_REUSEPORT failed.";
+		abort();
 	}
 #else
 	if (on) {
-		LOG_ERROR << "SO_REUSEPORT is not supported.";
+		// LOG_ERROR << "SO_REUSEPORT is not supported.";
+		abort();
 	}
 #endif
 }
@@ -47,3 +116,67 @@ void Socket::SetKeepAlive(bool on)
 		     static_cast<socklen_t>(sizeof optval));
 	// FIXME CHECK
 }
+
+int Socket::Write(std::string write_buffer)
+{
+	return Write(write_buffer.data(), write_buffer.size());
+}
+
+std::string Socket::Read()
+{
+	// TODO: Add error handling
+	char buf[4096];
+	auto nread = ::read(sockfd_, buf, 4096);
+	if (nread <= 0) {
+		return std::string();
+	} else {
+		return std::string(buf);
+	}
+}
+
+ssize_t Socket::ReadN(char *const buf, size_t n)
+{
+	size_t nleft;
+	ssize_t nread;
+	char *ptr;
+
+	ptr = (char*)buf;
+	nleft = n;
+	while (nleft > 0) {
+		if ((nread = read(sockfd_, ptr, nleft)) < 0) {
+			if (errno == EINTR)
+				nread = 0; /* and call read() again */
+			else
+				return (-1);
+		} else if (nread == 0)
+			break; /* EOF */
+
+		nleft -= nread;
+		ptr += nread;
+	}
+	return (n - nleft); /* return >= 0 */
+}
+
+int Socket::Write(const char write_buffer[], ssize_t nbytes)
+{
+	size_t nleft;
+	ssize_t nwritten;
+	const char *ptr;
+
+	ptr = write_buffer;
+	nleft = nbytes;
+	while (nleft > 0) {
+		if ((nwritten = write(sockfd_, ptr, nleft)) <= 0) {
+			if (nwritten < 0 && errno == EINTR)
+				nwritten = 0; /* and call write() again */
+			else
+				return -1; /* error */
+		}
+
+		nleft -= nwritten;
+		ptr += nwritten;
+	}
+	return nbytes;
+}
+
+} // nepenthe
--- others/utils/cpp/socket/socket.h
+++ others/utils/cpp/socket/socket.h
@@ -1,17 +1,47 @@
+// Author : Jack Drogon
+// Mail   : jack.xsuperman@gmail.com
+
+
 #ifndef SOCKET_H_
 #define SOCKET_H_
+
+#include <memory>
+#include <sys/socket.h>
+
+namespace nepenthe {
+
+class InetAddress;
+
+// TODO: right reference
+// TODO: noncopy
 
 class Socket {
 public:
 	explicit Socket(int sockfd) : sockfd_(sockfd) {}
+	Socket(int type, int protocol);
 	~Socket();
 
 	void SetTcpNoDelay(bool on);
 	void SetReuseAddr(bool on);
 	void SetReusePort(bool on);
 	void SetKeepAlive(bool on);
+
+	int Close();
+	int Connect(const InetAddress &address);
+	int Bind(const InetAddress &address);
+	int Listen(int backlog = SOMAXCONN);
+	std::shared_ptr<Socket> Accept(InetAddress &client_address);
+
+	int Write(std::string write_buffer);
+	int Write(const char write_buffer[], ssize_t len);
+	std::string Read();
+	ssize_t ReadN(char *const buf, size_t n);
+
+	int SockFd() const { return sockfd_; }
 private:
 	const int sockfd_;
 };
+
+} // nepenthe
 
 #endif // SOCKET_H_
