c60efdf0c5aa3f1ea120df15286ce62e887a4dcb
JackDrogon <jack.xsuperman@gmail.com>
2016-06-14 17:17:07 +0800

    Add cpp singleton template

---
+++ others/utils/cpp/design_pattern/singleton/singleton.h
@@ -0,0 +1,31 @@
+#include <memory>
+
+namespace nepenthe {
+
+namespace design_pattern {
+
+// @doc: can be deliver
+template<typename Type>
+class Singleton {
+public:
+	static std::shared_ptr<Type> Instance()
+	{
+		if (instance_.get() == nullptr) {
+			instance_.reset(new Type());
+		}
+
+		return instance_;
+	}
+
+protected:
+	Singleton();
+private:
+	static std::shared_ptr<Type> instance_;
+};
+
+template<typename Type>
+std::shared_ptr<Type> Singleton<Type>::instance_;
+
+} // design_pattern
+
+} // nepenthe
