d1ccbef9e757131936863da33d9ff504697dd5e1
JackDrogon <jack.xsuperman@gmail.com>
2016-12-20 11:45:48 +0800

    Add golang utils

---
+++ others/utils/golang/utils.go
@@ -0,0 +1,21 @@
+package utils
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func printUsageErrorAndExit(format string, values ...interface{}) {
+	fmt.Fprintf(os.Stderr, "ERROR: %s\n", fmt.Sprintf(format, values...))
+	fmt.Fprintln(os.Stderr)
+	fmt.Fprintln(os.Stderr, "Available command line options:")
+	flag.PrintDefaults()
+	os.Exit(64)
+}
+
+func printErrorAndExit(code int, format string, values ...interface{}) {
+	fmt.Fprintf(os.Stderr, "ERROR: %s\n", fmt.Sprintf(format, values...))
+	fmt.Fprintln(os.Stderr)
+	os.Exit(code)
+}
