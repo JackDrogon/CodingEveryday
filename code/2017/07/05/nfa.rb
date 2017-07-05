1b53de312aa22f367876dcbdd7307d0df6aee2a3
JackDrogon <jack.xsuperman@gmail.com>
2017-07-05 16:14:11 +0800

    Add nfa sample

---
+++ code/2017/07/05/nfa.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+# -*- coding:utf-8 -*-
+
+class FARule < Struct.new(:state, :character, :next_state)
+  def applies_to?(state, character)
+    self.state == state && self.character == character
+  end
+
+  def follow
+    next_state
+  end
+
+  def inspect
+    "#<FARuler #{state.inspect} -- #{character}--> #{next_state.inspect}>"
+  end
+end
+
+class DFARulebook < Struct.new(:rules)
+  def next_state(state, character)
+    rule_for(state, character).follow
+  end
+
+private
+  def rule_for(state, character)
+    rules.detect { |rule| rule.applies_to?(state, character) }
+  end
+end
+
+class DFA < Struct.new(:current_state, :accept_states, :rulebook)
+  def accepting?
+    accept_states.inlcude?(current_state)
+  end
+
+  def read_character(character)
+    self.current_state = rulebook.next_state(current_state, character)
+  end
+
+  def read_string(string)
+    string.chars.each do |ch|
+      read_character(ch)
+    end
+  end
+end
+
+class DFADesign < Struct.new(:start_state, :accept_states, :rulebook)
+  def to_dfa
+    DFA.new(start_state, accept_states, rulebook)
+  end
+
+  def accepts(string)
+    to_dfa.tap { |dfa| dfa.read_string(string) }.accepting?
+  end
+end
