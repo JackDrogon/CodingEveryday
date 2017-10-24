ecb265f495cd39755ae4facaaa5e91a28bffed93
Jack Drogon <jack.xsuperman@gmail.com>
2017-10-24 16:04:48 +0800

    Update go utils

--- others/utils/golang/utils.go
+++ others/utils/golang/utils.go
@@ -3,10 +3,11 @@ package utils
 import (
 	"flag"
 	"fmt"
+	"io"
 	"os"
 )
 
-func printUsageErrorAndExit(format string, values ...interface{}) {
+func PrintUsageErrorAndExit(format string, values ...interface{}) {
 	fmt.Fprintf(os.Stderr, "ERROR: %s\n", fmt.Sprintf(format, values...))
 	fmt.Fprintln(os.Stderr)
 	fmt.Fprintln(os.Stderr, "Available command line options:")
@@ -14,8 +15,33 @@ func printUsageErrorAndExit(format string, values ...interface{}) {
 	os.Exit(64)
 }
 
-func printErrorAndExit(code int, format string, values ...interface{}) {
+func PrintErrorAndExit(code int, format string, values ...interface{}) {
 	fmt.Fprintf(os.Stderr, "ERROR: %s\n", fmt.Sprintf(format, values...))
 	fmt.Fprintln(os.Stderr)
 	os.Exit(code)
+}
+
+// Read all file contents
+func FileContents(filename string) (string, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return "", err
+	}
+
+	defer f.Close()
+
+	var result []byte
+	buf := make([]byte, 4096)
+	for {
+		n, err := f.Read(buf[0:])
+		result = append(result, buf[0:n]...)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return "", err
+		}
+	}
+
+	return string(result), nil
 }
