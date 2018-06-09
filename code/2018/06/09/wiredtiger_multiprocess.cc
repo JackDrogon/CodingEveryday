b020dc7b687852ec34af7b9ec63bbad0652e8cc7
Jack Drogon <jack.xsuperman@gmail.com>
2018-06-09 10:17:52 +0800

    Add wiredtiger multiprocess sample

---
+++ code/2018/06/09/wiredtiger_multiprocess.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <sys/stat.h>
+
+#include <wiredtiger.h>
+
+using namespace std;
+
+#define error_check(call)                                                      \
+	do {                                                                   \
+		int __r;                                                       \
+		if ((__r = (call)) != 0 && __r != ENOTSUP)                     \
+			abort();                                               \
+	} while (0)
+
+const char *example_setup(int argc, char *argv[])
+{
+	const char *home;
+	(void)argc; /* Unused variable */
+	(void)argv;
+	if ((home = getenv("WIREDTIGER_HOME")) == NULL) {
+		home = "WT_HOME";
+	}
+	mkdir(home, 0755);
+
+	return home;
+}
+
+int main(int argc, char *argv[])
+{
+	const char *home;
+
+	WT_CONNECTION *conn;
+	WT_SESSION *session;
+
+	home = example_setup(argc, argv);
+
+	// XXX: If first one do not open with multiprocess, what will happen.
+
+	/*! [processes] */
+	/* Open a connection to the database, creating it if necessary. */
+	error_check(wiredtiger_open(home, NULL, "create,multiprocess", &conn));
+
+	/* Open a session for the current thread's work. */
+	error_check(conn->open_session(conn, NULL, NULL, &session));
+
+	/* XXX Do some work... */
+
+	/* Note: closing the connection implicitly closes open session(s). */
+	error_check(conn->close(conn, NULL));
+	/*! [processes] */
+
+	return (EXIT_SUCCESS);
+}
