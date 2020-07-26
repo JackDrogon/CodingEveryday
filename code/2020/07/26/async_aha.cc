3f1d0b9032e4de27abe37b76b4eb7e6fee551d74
Jack Drogon <jack.xsuperman@gmail.com>
2020-07-26 10:53:28 +0800

    Add aha: cpp async

---
+++ code/2020/07/26/async_aha.cc
@@ -0,0 +1,38 @@
+#include <chrono>
+#include <future>
+#include <iostream>
+#include <random>
+#include <thread>
+using namespace std;
+
+namespace
+{
+static int run(char c)
+{
+	default_random_engine dre(static_cast<unsigned int>(c));
+	uniform_int_distribution<int> id(2, 30);
+
+	for (size_t i = 0; i < 10; ++i) {
+		this_thread::sleep_for(chrono::milliseconds(id(dre)));
+		cout.put(c).flush();
+	}
+
+	return static_cast<int>(c);
+}
+
+int f1() { return run('.'); }
+int f2() { return run('+'); }
+} // namespace
+
+int main()
+{
+	cout << "starting f1() '.' in background"
+	     << " and f2() '+' in foreground:" << endl;
+
+	auto result1_future = async(f1);
+	int result2 = f2();
+
+	auto result = result1_future.get() + result2;
+
+	cout << "\nresult of f1() + f2(): " << result << endl;
+}
