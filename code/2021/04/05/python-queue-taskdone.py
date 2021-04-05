d173bcbafac2e4eb076c33d14186f1351e7da4e8
Jack Drogon <jack.xsuperman@gmail.com>
2021-04-05 08:05:13 +0800

    Add python Queue taskdone

---
+++ code/2021/04/05/python-queue-taskdone.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import queue
+
+
+def main():
+    q = queue.Queue()
+    for i in range(3):
+        q.put(i)
+    for i in range(3):
+        print(q.get())
+        q.task_done()
+    q.join()
+
+
+if __name__ == "__main__":
+    main()
