311cbce5bd8f112558780d947e6b5f900b0e25df
Jack Drogon <jack.xsuperman@gmail.com>
2021-04-11 08:31:19 +0800

    Add python iter usage sample

---
+++ code/2021/04/11/sentence.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import re
+import reprlib
+
+
+RE_WORD = re.compile('\w+')
+
+
+class Sentence:
+    def __init__(self, text):
+        self.text = text
+        self.words = RE_WORD.findall(text)
+
+    def __getitem__(self, index):
+        return self.words[index]
+
+    def __len__(self):
+        return len(self.words)
+
+    def __repr__(self):
+        return f'Sentence({reprlib.repr(self.text)})'
