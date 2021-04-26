82e7af679061888a3f0b05e01a70ecb3f0553298
Jack Drogon <jack.xsuperman@gmail.com>
2021-04-27 07:29:18 +0800

    Add python heapq sample

---
+++ code/2021/04/27/python-heapq.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import heapq
+
+
+def nlargest_nsmallest():
+    nums = [1, 8, 2, 23, 7, -4, 18, 23, 42, 37, 2]
+    print(heapq.nlargest(3, nums))  # Prints [42, 37, 23]
+    print(heapq.nsmallest(3, nums))  # Prints [-4, 1, 2]
+
+
+def nlargest_nsmallest_with_keyfunc():
+    portfolio = [{
+        'name': 'IBM',
+        'shares': 100,
+        'price': 91.1
+    }, {
+        'name': 'AAPL',
+        'shares': 50,
+        'price': 543.22
+    }, {
+        'name': 'FB',
+        'shares': 200,
+        'price': 21.09
+    }, {
+        'name': 'HPQ',
+        'shares': 35,
+        'price': 31.75
+    }, {
+        'name': 'YHOO',
+        'shares': 45,
+        'price': 16.35
+    }, {
+        'name': 'ACME',
+        'shares': 75,
+        'price': 115.65
+    }]
+    cheap = heapq.nsmallest(3, portfolio, key=lambda s: s['price'])
+    print(cheap)
+    expensive = heapq.nlargest(3, portfolio, key=lambda s: s['price'])
+    print(expensive)
+
+
+def main():
+    nlargest_nsmallest()
+    nlargest_nsmallest_with_keyfunc()
+
+
+if __name__ == "__main__":
+    main()
