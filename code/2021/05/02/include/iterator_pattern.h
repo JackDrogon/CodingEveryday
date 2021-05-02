388b4a027b8514c5573effc3cfe7b36be2a2628f
Jack Drogon <jack.xsuperman@gmail.com>
2021-05-02 08:11:20 +0800

    Add cmake prj with iterator pattern

---
+++ code/2021/05/02/include/iterator_pattern.h
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
+
+template <typename Object> class Iterator {
+    public:
+	Iterator() = default;
+	virtual ~Iterator() = default;
+
+	virtual bool HasNext() const = 0;
+	virtual Object &Next() = 0;
+};
+
+template <typename Object> class Aggregate {
+    public:
+	Aggregate() = default;
+	virtual ~Aggregate() = default;
+
+	virtual std::unique_ptr<Iterator<Object>> NewIterator() = 0;
+};
+
+class Book {
+    public:
+	Book(std::string name) : name_(std::move(name))
+	{
+	}
+
+	std::string Name() const
+	{
+		return name_;
+	}
+
+    private:
+	std::string name_;
+};
+
+class BookShelf final : public Aggregate<Book> {
+    public:
+	BookShelf() = default;
+
+	auto GetSize() const
+	{
+		return books_.size();
+	}
+
+	// TODO: const
+	[[nodiscard]] auto GetBookAt(size_t index) -> Book &
+	{
+		return books_[index];
+	}
+
+	[[nodiscard]] auto GetBookAt(size_t index) const -> const Book &
+	{
+		return books_[index];
+	}
+
+	void AppendBook(std::string name)
+	{
+		books_.emplace_back(std::move(name));
+	}
+
+	virtual std::unique_ptr<Iterator<Book>> NewIterator() override;
+
+    private:
+	std::vector<Book> books_;
+};
+
+class BookShelfIterator final : public Iterator<Book> {
+    public:
+	BookShelfIterator(BookShelf *bookself)
+		: current_index_(0), bookself_(bookself)
+	{
+	}
+
+	bool HasNext() const override;
+	Book &Next() override;
+
+    private:
+	size_t current_index_;
+	BookShelf *bookself_;
+};
---
+++ code/2021/05/02/src/iterator_pattern_unittest.cc
@@ -0,0 +1,86 @@
+#include "iterator_pattern.h"
+
+#include <vector>
+#include <string>
+
+#include <catch2/catch.hpp>
+
+struct BookShelfCreator {
+	BookShelfCreator()
+		: booknames{ "Around the World in 80 Days", "Bible",
+			     "Cinderella", "Daddy-Long-Legs" }
+	{
+		for (auto &&name : booknames) {
+			bookshelf.AppendBook(name);
+		}
+	}
+
+	std::vector<std::string> booknames;
+	BookShelf bookshelf;
+};
+
+TEST_CASE("GetSize", "[BookShelf]")
+{
+	SECTION("mutable bookshelf")
+	{
+		BookShelf bookshelf;
+		REQUIRE(bookshelf.GetSize() == 0);
+
+		bookshelf.AppendBook("hello");
+		REQUIRE(bookshelf.GetSize() == 1);
+	}
+
+	SECTION("const bookshelf")
+	{
+		const BookShelf bookshelf;
+		REQUIRE(bookshelf.GetSize() == 0);
+	}
+}
+
+TEST_CASE("GetBookAt", "[BookShelf]")
+{
+	BookShelfCreator bookshelf_creator;
+	const auto &[booknames, bookshelf] = bookshelf_creator;
+
+	for (size_t idx = 0; idx < booknames.size(); ++idx) {
+		REQUIRE(bookshelf.GetBookAt(idx).Name() == booknames[idx]);
+	}
+}
+
+TEST_CASE("AppendBook", "[BookShelf]")
+{
+	std::vector<std::string> v{ "Around the World in 80 Days", "Bible",
+				    "Cinderella", "Daddy-Long-Legs" };
+	BookShelf bookshelf;
+	for (auto &&name : v) {
+		bookshelf.AppendBook(name);
+	}
+
+	REQUIRE(bookshelf.GetSize() == v.size());
+}
+
+TEST_CASE("NewIterator", "[BookShelf]")
+{
+	BookShelfCreator bookshelf_creator;
+	auto &[booknames, bookshelf] = bookshelf_creator;
+
+	auto iter = bookshelf.NewIterator();
+	REQUIRE(iter);
+
+	auto &firstbook = iter->Next();
+	REQUIRE(firstbook.Name() == booknames[0]);
+}
+
+TEST_CASE("Iterate", "[BookShelfIterator]")
+{
+	BookShelfCreator bookshelf_creator;
+	auto &[booknames, bookshelf] = bookshelf_creator;
+
+	size_t idx = 0;
+	auto iter = bookshelf.NewIterator();
+	while (iter->HasNext()) {
+		auto book = iter->Next();
+		REQUIRE(book.Name() == booknames[idx++]);
+	}
+	REQUIRE(idx == booknames.size());
+}
