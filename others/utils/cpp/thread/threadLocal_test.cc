d7b7b81c10d5a4052cc650ffc70944aa4b94f85d
Jack Drogon <jack.xsuperman@gmail.com>
2021-05-06 08:38:53 +0800

    Update repo clang-format && Refactor cpp utils thread_local

--- others/utils/cpp/thread/threadLocal_test.cc
+++ others/utils/cpp/thread/threadLocal_test.cc
@@ -7,9 +7,11 @@
 
 using namespace std;
 
+namespace
+{
 class Test
 {
-public:
+    public:
 	Test()
 	{
 		printf("tid=%d, constructing %p\n", pthread_self(), this);
@@ -17,47 +19,51 @@ public:
 
 	~Test()
 	{
-		printf("tid=%d, destructing %p %s\n", pthread_self(), this, name_.c_str());
+		printf("tid=%d, destructing %p %s\n", pthread_self(), this,
+		       name_.c_str());
 	}
 
-	const string& name() const { return name_; }
-	void setName(const string& n) { name_ = n; }
+	const string &name() const
+	{
+		return name_;
+	}
+	void setName(const string &n)
+	{
+		name_ = n;
+	}
 
-private:
+    private:
 	string name_;
 };
+} // namespace
 
 ThreadLocal<Test> testObj1;
 ThreadLocal<Test> testObj2;
 
 void print()
 {
-	printf("tid=%d, obj1 %p name=%s\n",
-			pthread_self(),
-			&testObj1.value(),
-			testObj1.value().name().c_str());
-	printf("tid=%d, obj2 %p name=%s\n",
-			pthread_self(),
-			&testObj2.value(),
-			testObj2.value().name().c_str());
+	printf("tid=%d, obj1 %p name=%s\n", pthread_self(), &testObj1.Value(),
+	       testObj1.Value().name().c_str());
+	printf("tid=%d, obj2 %p name=%s\n", pthread_self(), &testObj2.Value(),
+	       testObj2.Value().name().c_str());
 }
 
 void threadFunc()
 {
 	print();
-	testObj1.value().setName("changed 1");
-	testObj2.value().setName("changed 42");
+	testObj1.Value().setName("changed 1");
+	testObj2.Value().setName("changed 42");
 	print();
 }
 
 int main()
 {
-	testObj1.value().setName("main one");
+	testObj1.Value().setName("main one");
 	print();
 	thread t1(threadFunc);
 	t1.join();
 
-	testObj2.value().setName("main two");
+	testObj2.Value().setName("main two");
 	print();
 
 	pthread_exit(0);
--- others/utils/cpp/thread/thread_local.h
+++ others/utils/cpp/thread/thread_local.h
@@ -1,9 +1,11 @@
-#ifndef __THREAD_LOCAL_H__
-#define __THREAD_LOCAL_H__
+#pragma once
+
+#include <memory>
 
 #include <cassert>
-#include <sys/cdefs.h>
+
 #include <pthread.h>
+#include <sys/cdefs.h>
 
 #define __THROW throw()
 
@@ -14,7 +16,7 @@ __BEGIN_DECLS
 extern void __assert_perror_fail(int errnum, const char *file,
 				 unsigned int line,
 				 const char *function) __THROW
-    __attribute__((__noreturn__));
+	__attribute__((__noreturn__));
 __END_DECLS
 #endif
 
@@ -32,48 +34,54 @@ __END_DECLS
 	({                                                                     \
 		__typeof__(ret) errnum = (ret);                                \
 		assert(errnum == 0);                                           \
-		(void) errnum;                                                 \
+		(void)errnum;                                                  \
 	})
 
 #endif // CHECK_PTHREAD_RETURN_VALUE
 
 template <typename T> class ThreadLocal
 {
-      public:
+    public:
 	ThreadLocal()
 	{
 		MCHECK(pthread_key_create(&pkey_, &ThreadLocal::destructor));
 	}
 
-	~ThreadLocal() { MCHECK(pthread_key_delete(pkey_)); }
+	ThreadLocal(const ThreadLocal &); // = delete
+	ThreadLocal &operator=(const ThreadLocal &); // = delete
 
-	T &value()
+	~ThreadLocal()
 	{
-		T *perThreadValue =
-		    static_cast<T *>(pthread_getspecific(pkey_));
-		if (!perThreadValue) {
-			T *newObj = new T();
-			MCHECK(pthread_setspecific(pkey_, newObj));
-			perThreadValue = newObj;
-		}
-		return *perThreadValue;
+		MCHECK(pthread_key_delete(pkey_));
 	}
 
-      private:
+	T &Value()
+	{
+		T *per_thread_value =
+			static_cast<T *>(pthread_getspecific(pkey_));
+
+		if (!per_thread_value) {
+			auto new_obj = std::make_unique<T>();
+			MCHECK(pthread_setspecific(pkey_, new_obj));
+			per_thread_value = new_obj.release();
+		}
+
+		return *per_thread_value;
+	}
+
+    private:
 	static void destructor(void *x)
 	{
 		T *obj = static_cast<T *>(x);
+
+		// TODO: use enable_if
 		typedef char T_must_be_complete_type[sizeof(T) == 0 ? -1 : 1];
 		T_must_be_complete_type dummy;
 		(void)dummy;
+
 		delete obj;
 	}
 
-      private:
+    private:
 	pthread_key_t pkey_;
-
-	ThreadLocal(const ThreadLocal &);	    // = delete
-	ThreadLocal &operator=(const ThreadLocal &); // = delete
 };
-
-#endif // __THREAD_LOCAL_H__
